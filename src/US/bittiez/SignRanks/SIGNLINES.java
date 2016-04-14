299becb11dbb12a1e36fc8b08bd410287ad0145e
bittiez <bittiez@gmail.com>
2016-04-14 00:32:28 -0600

    Will now charge users(If an economy plugin exists) and will set the players group Issues: (Only tested in permissionEx) The group is set in the current world instead of global.

---
+++ src/US/bittiez/SignRanks/SIGNLINES.java
@@ -0,0 +1,11 @@
+package US.bittiez.SignRanks;
+
+/**
+ * Created by bittiez on 13/04/16.
+ */
+public class SIGNLINES {
+    public static int TITLE = 0;
+    public static int GROUP = 1;
+    public static int COST = 2;
+    public static int ID = 3;
+}
--- src/US/bittiez/SignRanks/main.java
+++ src/US/bittiez/SignRanks/main.java
@@ -1,11 +1,9 @@
 package US.bittiez.SignRanks;
 
-import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
-import org.bukkit.Bukkit;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
@@ -34,10 +32,13 @@ Sign setup:
 public class main extends JavaPlugin implements Listener{
     private static Logger log = Logger.getLogger("SignRanks");
     private static Economy economy = null;
+    private static Permission permission = null;
     private static ChatColor titlePrefix = ChatColor.DARK_BLUE;
 
     //Set up these in config later
     private String signTitle = "[SignRanks]";
+    private String changedGroup = "&2You have been &6promoted&2!";
+    private String notEnoughMoney = "&2You do not have enough &6money &2to continue.";
 
     @Override
     public void onEnable(){
@@ -52,6 +53,15 @@ public class main extends JavaPlugin implements Listener{
             } else
                 economy = rsp.getProvider();
         }
+
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        permission = rsp.getProvider();
+        if(permission == null){
+            log.warning("Could not find a permission plugin! SignRanks will not load without a permission plugin!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -61,13 +71,13 @@ public class main extends JavaPlugin implements Listener{
         String group, cost;
         if(who.hasPermission("SignRanks.create")) {
             if (!sign.isCancelled()) {
-                if (sign.getLine(0).equalsIgnoreCase(signTitle)) {
-                    if (!(group = sign.getLine(1)).isEmpty()) {
-                        cost = sign.getLine(2);
+                if (sign.getLine(SIGNLINES.TITLE).equalsIgnoreCase(signTitle)) {
+                    if (!(group = sign.getLine(SIGNLINES.GROUP)).isEmpty()) {
+                        cost = sign.getLine(SIGNLINES.COST);
                         Random r = new Random();
                         String id = r.nextInt(10000) + "";
-                        sign.setLine(0, titlePrefix + signTitle);
-                        sign.setLine(3, ChatColor.MAGIC + id);
+                        sign.setLine(SIGNLINES.TITLE, titlePrefix + signTitle);
+                        sign.setLine(SIGNLINES.ID, ChatColor.MAGIC + id);
                     }
                 }
             }
@@ -106,15 +116,12 @@ public class main extends JavaPlugin implements Listener{
 
 
     private boolean isSignRankSign(Sign sign){
-        if(sign.getLine(0).equalsIgnoreCase(titlePrefix + signTitle))
-            if(!sign.getLine(1).isEmpty())
-                if(!sign.getLine(3).isEmpty())
-                    if(sign.getLine(3).startsWith(ChatColor.MAGIC + ""))
-                        return true ;
-                    else
-                        log.warning("Err 1");
+        if(sign.getLine(SIGNLINES.TITLE).equalsIgnoreCase(titlePrefix + signTitle))
+            if(!sign.getLine(SIGNLINES.GROUP).isEmpty())
+                if(sign.getLine(SIGNLINES.ID).startsWith(ChatColor.MAGIC + ""))
+                    return true ;
                 else
-                    log.warning("Err 2");
+                    log.warning("Err 1");
             else
                 log.warning("Err 3");
         else
@@ -125,14 +132,31 @@ public class main extends JavaPlugin implements Listener{
 
     private void handleSignClick(Player who, Sign sign){
         if(who.hasPermission("SignRanks.use")){
-            who.sendMessage(ChatColor.DARK_PURPLE + "Successfully used a SignRanks sign! ( "+sign.getLine(1)+", "+ChatColor.stripColor(sign.getLine(3))+" )");
+            String group = sign.getLine(SIGNLINES.GROUP);
+            Double cost = Double.parseDouble(sign.getLine(SIGNLINES.COST));
+            String id = sign.getLine(SIGNLINES.ID);
+            boolean proceed = false;
+            if(cost > 0) {
+                if (purchase(who, cost)) {
+                    proceed = true;
+                } else
+                    who.sendMessage(ChatColor.translateAlternateColorCodes('&', notEnoughMoney));
+            } else
+                proceed = true;
+
+            if(proceed){
+                //permission.playerRemoveGroup(who, permission.getPrimaryGroup(who));
+                permission.playerAddGroup(who, group);
+                who.sendMessage(ChatColor.translateAlternateColorCodes('&', changedGroup));
+            }
         } else {
             who.sendMessage(ChatColor.RED + "You do not have permission to use this sign!");
             log.info(who.getName() + " tried to use a SignRanks sign, but does not have the SignRanks.use permission.");
         }
     }
 
-    private boolean purchase(Player who, int cost){
+
+    private boolean purchase(Player who, double cost){
         if(economy != null) {
             if (economy.getBalance(who) >= cost) {
                 EconomyResponse er = economy.withdrawPlayer(who, cost);
@@ -141,5 +165,8 @@ public class main extends JavaPlugin implements Listener{
             }
         }
         return false;
+    }
+    private boolean purchase(Player who, int cost){
+        return purchase(who, (double)cost);
     }
 }
