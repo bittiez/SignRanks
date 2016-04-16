5d27869a0ea316d1f7654fe8552dba2d762f8f4e
bittiez <bittiez@gmail.com>
2016-04-16 00:55:51 -0600

    Many changes -Signs created are now saved in signData.yml -Signs are identified by a random generated number between 1-100,000 (Used on the last line of the sign, masked with the Magic text format -Switched from hard coded text messages to a config.yml file -Permissions are now set on a global level instead of per world

--- src/US/bittiez/SignRanks/main.java
+++ src/US/bittiez/SignRanks/main.java
@@ -6,16 +6,22 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.WorldSaveEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -34,11 +40,11 @@ public class main extends JavaPlugin implements Listener{
     private static Economy economy = null;
     private static Permission permission = null;
     private static ChatColor titlePrefix = ChatColor.DARK_BLUE;
+    private FileConfiguration mainConfig = getConfig();
+    private FileConfiguration signData;
 
-    //Set up these in config later
-    private String signTitle = "[SignRanks]";
-    private String changedGroup = "&2You have been &6promoted&2!";
-    private String notEnoughMoney = "&2You do not have enough &6money &2to continue.";
+    //Options set in the config
+    private String signTitle, changedGroup, notEnoughMoney, failedToChangePermission;
 
     @Override
     public void onEnable(){
@@ -63,9 +69,20 @@ public class main extends JavaPlugin implements Listener{
         }
 
         getServer().getPluginManager().registerEvents(this, this);
+        loadConfig();
+        loadSignData();
     }
 
     @EventHandler
+    public void onWorldSave(WorldSaveEvent event) {
+        try {
+            signData.save(new File(this.getDataFolder(), "signData.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        @EventHandler
     public void main(SignChangeEvent sign){
         Player who = sign.getPlayer();
         String group, cost;
@@ -75,9 +92,15 @@ public class main extends JavaPlugin implements Listener{
                     if (!(group = sign.getLine(SIGNLINES.GROUP)).isEmpty()) {
                         cost = sign.getLine(SIGNLINES.COST);
                         Random r = new Random();
-                        String id = r.nextInt(10000) + "";
+                        String id = r.nextInt(100000) + "";
                         sign.setLine(SIGNLINES.TITLE, titlePrefix + signTitle);
                         sign.setLine(SIGNLINES.ID, ChatColor.MAGIC + id);
+
+                        String[] signInfo = {
+                                group, cost, "commands: /spawn, /spawn"
+                        };
+
+                        signData.set("signs." + id, signInfo);
                     }
                 }
             }
@@ -113,7 +136,30 @@ public class main extends JavaPlugin implements Listener{
 
 
 
+    private void loadSignData(){
+        File signFile = new File(this.getDataFolder(), "signData.yml");
+        if(!signFile.exists()) {
+            try {
+                signFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(signFile.exists())
+            signData = YamlConfiguration.loadConfiguration(signFile);
+        else
+            getServer().getPluginManager().disablePlugin(this);
+    }
 
+    private void loadConfig(){
+        mainConfig.options().copyDefaults();
+        saveDefaultConfig();
+
+        signTitle = mainConfig.getString("signTitle");
+        changedGroup = mainConfig.getString("changedGroup");
+        notEnoughMoney = mainConfig.getString("notEnoughMoney");
+        failedToChangePermission = mainConfig.getString("failedToChangePermission");
+    }
 
     private boolean isSignRankSign(Sign sign){
         if(sign.getLine(SIGNLINES.TITLE).equalsIgnoreCase(titlePrefix + signTitle))
@@ -145,9 +191,18 @@ public class main extends JavaPlugin implements Listener{
                 proceed = true;
 
             if(proceed){
-                //permission.playerRemoveGroup(who, permission.getPrimaryGroup(who));
-                permission.playerAddGroup(who, group);
-                who.sendMessage(ChatColor.translateAlternateColorCodes('&', changedGroup));
+                String prevGroup = permission.getPrimaryGroup(who);
+                if(permission.playerAddGroup(null, who, group)) {
+                    who.sendMessage(ChatColor.translateAlternateColorCodes('&', changedGroup));
+                    permission.playerRemoveGroup(null, who, prevGroup);
+                }
+                else {
+                    who.sendMessage(ChatColor.translateAlternateColorCodes('&', failedToChangePermission));
+                    log.warning(who.getName() + " tried to use a SignRanks sign to change their group, there was an error changing their permission." +
+                            String.format("[Who: %1] [Sign Location: %2] [Attempted Group: %3] [Sign ID: %4]", who.getName(), sign.getLocation().toString(), group, id));
+                    if(cost > 0)
+                        refund(who, cost);
+                }
             }
         } else {
             who.sendMessage(ChatColor.RED + "You do not have permission to use this sign!");
@@ -155,6 +210,15 @@ public class main extends JavaPlugin implements Listener{
         }
     }
 
+    private boolean refund(Player who, double cost){
+        if(economy != null)
+        {
+            EconomyResponse er = economy.depositPlayer(who, cost);
+            if(er.transactionSuccess())
+                return true;
+        }
+        return false;
+    }
 
     private boolean purchase(Player who, double cost){
         if(economy != null) {
